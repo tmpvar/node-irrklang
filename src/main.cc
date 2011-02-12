67400e52f392323ecc142d9fd5bf791b9ebd8c64
Elijah Insua <tmpvar@gmail.com>
2011-02-12 14:31:16 -0500

    exporting irrKlang constructor

--- src/main.cc
+++ src/main.cc
@@ -2,12 +2,12 @@
 #include <node.h>
 #include <irrKlang.h>
 
-using namespace irrklang;
+
 using namespace node;
 using namespace v8;
+using namespace irrklang;
 
-/*
-class irrKlang {
+/*class irrKlang {
   pubic:
     static void
     Initialize (v8::Handle<v8::Object> target)
@@ -16,21 +16,9 @@ class irrKlang {
 
       Local<FunctionTemplate> t = FunctionTemplate::New(New);
     }
-};
+};*/
 
-
-Handle<Value> play(const Arguments& args) {
-  HandleScope scope;
-
-   // play a single sound
-   engine->play2D("../../media/bell.wav");
-
-  return ThrowException(Exception::Error(
-  String::New("glGetShaderInfoLog is not implemented!")));
-}
-*/
-
-class Irrkl: ObjectWrap
+class irrKlang: ObjectWrap
 {
 private:
   int m_count;
@@ -45,27 +33,27 @@ public:
 
     s_ct = Persistent<FunctionTemplate>::New(t);
     s_ct->InstanceTemplate()->SetInternalFieldCount(1);
-    s_ct->SetClassName(String::NewSymbol("Irrkl"));
+    s_ct->SetClassName(String::NewSymbol("irrKlang"));
 
     NODE_SET_PROTOTYPE_METHOD(s_ct, "play", Play);
 
-    target->Set(String::NewSymbol("Irrkl"),
+    target->Set(String::NewSymbol("irrKlang"),
                 s_ct->GetFunction());
   }
 
-  Irrkl() :
+  irrKlang() :
     m_count(0)
   {
   }
 
-  ~Irrkl()
+  ~irrKlang()
   {
   }
 
   static Handle<Value> New(const Arguments& args)
   {
     HandleScope scope;
-    Irrkl* hw = new Irrkl();
+    irrKlang* hw = new irrKlang();
     hw->Wrap(args.This());
     return args.This();
   }
@@ -73,7 +61,7 @@ public:
   static Handle<Value> Play(const Arguments& args)
   {
     HandleScope scope;
-    Irrkl* hw = ObjectWrap::Unwrap<Irrkl>(args.This());
+    irrKlang* hw = ObjectWrap::Unwrap<irrKlang>(args.This());
     hw->m_count++;
     ISoundEngine* engine = createIrrKlangDevice();
     if (!engine)
@@ -88,18 +76,8 @@ public:
 
 };
 
-Persistent<FunctionTemplate> Irrkl::s_ct;
+Persistent<FunctionTemplate> irrKlang::s_ct;
 
-extern "C" {
-  static void init (Handle<Object> target) {
-  
-    //Local<FunctionTemplate> play = FunctionTemplate::New(gl_glVertex2dv);
-    //target->Set(String::New("glVertex2dv"), play->GetFunction());
-
-    // HandleScope scope;
-    Irrkl::Init(target);
-
-  }
-  
-  NODE_MODULE(Irrkl, init);  
+extern "C" void init (Handle<Object> target) {
+  irrKlang::Init(target);
 }
--- test/sanity.js
+++ test/sanity.js
@@ -1,1 +1,4 @@
-console.log(require(__dirname + "/../build/default/node-irrklang"));
+var irrKlang = require(__dirname + "/../build/default/node-irrklang").irrKlang,
+    player   = new irrKlang();
+
+console.log(player.Play);
